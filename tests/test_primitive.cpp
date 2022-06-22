d57facc01c9cd6e1afed42303f59ff5c42407f87
Jayden Chan <jaydencn7@gmail.com>
2022-06-21 20:19:24 -0700

    JC: Clean up primitives tests

--- tests/test_primitive.cpp
+++ tests/test_primitive.cpp
@@ -20,45 +20,49 @@
 #include "ray.hpp"
 #include "vec3.hpp"
 
-TEST_CASE("path_tracer::Ray/Sphere intersection", "[primitive][ray][sphere]") {
-  // Sphere at the origin on the XY plane
-  auto const center = path_tracer::Vec3(0, 0, 0);
-  auto const radius = 5.0F;
-  auto const sphere = path_tracer::Sphere(center, radius);
+using path_tracer::Ray;
+using path_tracer::Sphere;
+using path_tracer::Triangle;
+using path_tracer::Vec3;
 
-  auto const t_min = 0;
-  auto const t_max = 10000000000;
+TEST_CASE("Ray/Sphere intersection", "[primitive][ray][sphere]") {
+  // Sphere at the origin on the XY plane
+  const auto center = Vec3(0, 0, 0);
+  const auto radius = 5.0F;
+  const auto sphere = Sphere(center, radius);
+
+  const auto t_min = 0;
+  const auto t_max = 10000000000;
 
   // send a ray through the origin and ensure it hits
-  auto ray_origin = path_tracer::Vec3(0, 0, -10);
-  auto dir = (path_tracer::Vec3(0, 0, 0) - ray_origin).normalize();
-  auto ray = path_tracer::Ray(ray_origin, dir);
+  auto ray_origin = Vec3(0, 0, -10);
+  auto dir = (Vec3(0, 0, 0) - ray_origin).normalize();
+  auto ray = Ray(ray_origin, dir);
   REQUIRE(sphere.hit(ray, t_min, t_max));
 
-  ray_origin = path_tracer::Vec3(0, -5.000001F, -10);
-  dir = (path_tracer::Vec3(0, -5.000001F, 0) - ray_origin).normalize();
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(0, -5.000001F, -10);
+  dir = (Vec3(0, -5.000001F, 0) - ray_origin).normalize();
+  ray = Ray(ray_origin, dir);
   REQUIRE(!sphere.hit(ray, t_min, t_max));
 
-  ray_origin = path_tracer::Vec3(0, -4.999999F, -10);
-  dir = (path_tracer::Vec3(0, -4.999999F, 0) - ray_origin).normalize();
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(0, -4.999999F, -10);
+  dir = (Vec3(0, -4.999999F, 0) - ray_origin).normalize();
+  ray = Ray(ray_origin, dir);
   REQUIRE(sphere.hit(ray, t_min, t_max));
 
-  ray_origin = path_tracer::Vec3(4, 4, -10);
-  dir = (path_tracer::Vec3(4, 4, 0) - ray_origin).normalize();
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(4, 4, -10);
+  dir = (Vec3(4, 4, 0) - ray_origin).normalize();
+  ray = Ray(ray_origin, dir);
   REQUIRE(!sphere.hit(ray, t_min, t_max));
 
   // generate a bunch of random rays through the origin and
   // ensure they all hit
   for (int i = 0; i < 10; i++) {
-    auto const rand_point = path_tracer::Vec3::rand() * 1000;
-    auto const dir_to_origin =
-        (path_tracer::Vec3(0, 0, 0) - rand_point).normalize();
-    auto const r = path_tracer::Ray(rand_point, dir_to_origin);
+    const auto rand_point = Vec3::rand() * 1000;
+    const auto dir_to_origin = (Vec3(0, 0, 0) - rand_point).normalize();
+    const auto r = Ray(rand_point, dir_to_origin);
     INFO("Expecting vec to hit: " << r);
-    auto const hit = sphere.hit(r, t_min, t_max);
+    const auto hit = sphere.hit(r, t_min, t_max);
     REQUIRE(hit);
   }
 
@@ -66,73 +70,89 @@ TEST_CASE("path_tracer::Ray/Sphere intersection", "[primitive][ray][sphere]") {
   // technically there is a chance that one of these could hit but
   // it's very unlikely. probably still bad testing practice but oh well
   for (int i = 0; i < 10; i++) {
-    auto const r1 = path_tracer::Vec3::rand() * 10000;
-    auto const r2 = path_tracer::Vec3::rand() * 10000;
-    auto const r_dir = (r1 - r2).normalize();
-    auto const r = path_tracer::Ray(r1, r_dir);
+    const auto r1 = Vec3::rand() * 10000;
+    const auto r2 = Vec3::rand() * 10000;
+    const auto r_dir = (r1 - r2).normalize();
+    const auto r = Ray(r1, r_dir);
     INFO("Expecting vec to miss: " << r);
-    auto const hit = sphere.hit(r, t_min, t_max);
+    const auto hit = sphere.hit(r, t_min, t_max);
     REQUIRE(!hit);
   }
 }
 
-TEST_CASE("path_tracer::Ray/Triangle intersection",
-          "[primitive][ray][triangle]") {
-  // Triangle at the origin on the XY plane
-  auto const v0 = path_tracer::Vec3(0, 5, 0);
-  auto const v1 = path_tracer::Vec3(-5, -5, 0);
-  auto const v2 = path_tracer::Vec3(5, -5, 0);
-  auto const triangle = path_tracer::Triangle(v0, v1, v2, -1);
+TEST_CASE("Ray/Sphere internal intersection", "[primitive][ray][sphere]") {
+  // Sphere at the origin on the XY plane
+  const auto center = Vec3(0, 0, 0);
+  const auto radius = 5.0F;
+  const auto sphere = Sphere(center, radius);
 
-  auto const t_min = 0;
-  auto const t_max = 10000000000;
+  const auto t_min = 0;
+  const auto t_max = 10000000000;
+
+  const auto ray_origin = Vec3(0, 0, 0);
+  const auto dir = Vec3(0, 0, -1);
+  const auto ray = Ray(ray_origin, dir);
+  const auto hit = sphere.hit(ray, t_min, t_max);
+  REQUIRE(hit.has_value());
+  REQUIRE(hit->point == Vec3(0, 0, -5));
+  REQUIRE(hit->normal == Vec3(0, 0, -1));
+}
+
+TEST_CASE("Ray/Triangle intersection", "[primitive][ray][triangle]") {
+  // Triangle at the origin on the XY plane
+  const auto v0 = Vec3(0, 5, 0);
+  const auto v1 = Vec3(-5, -5, 0);
+  const auto v2 = Vec3(5, -5, 0);
+  const auto triangle = Triangle(v0, v1, v2, -1);
+
+  const auto t_min = 0;
+  const auto t_max = 10000000000;
 
   // send a ray through the origin and ensure it hits
-  auto ray_origin = path_tracer::Vec3(0, 0, -10);
-  auto dir = (path_tracer::Vec3(0, 0, 0) - ray_origin).normalize();
-  auto ray = path_tracer::Ray(ray_origin, dir);
+  auto ray_origin = Vec3(0, 0, -10);
+  auto dir = (Vec3(0, 0, 0) - ray_origin).normalize();
+  auto ray = Ray(ray_origin, dir);
   REQUIRE(triangle.hit(ray, t_min, t_max));
 
   // send some more manually-tuned rays and check their result
-  ray_origin = path_tracer::Vec3(2, 2, -10);
-  dir = (path_tracer::Vec3(2, 2, 0) - ray_origin).normalize();
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(2, 2, -10);
+  dir = (Vec3(2, 2, 0) - ray_origin).normalize();
+  ray = Ray(ray_origin, dir);
   REQUIRE(!triangle.hit(ray, t_min, t_max));
 
-  ray_origin = path_tracer::Vec3(-4.9999F, -4.9999F, -10);
-  dir = (path_tracer::Vec3(-4.9999F, -4.9999F, 0) - ray_origin).normalize();
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(-4.9999F, -4.9999F, -10);
+  dir = (Vec3(-4.9999F, -4.9999F, 0) - ray_origin).normalize();
+  ray = Ray(ray_origin, dir);
   REQUIRE(triangle.hit(ray, t_min, t_max));
 
-  ray_origin = path_tracer::Vec3(10, 0, 10);
-  dir = path_tracer::Vec3(-1, 0, 0);
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(10, 0, 10);
+  dir = Vec3(-1, 0, 0);
+  ray = Ray(ray_origin, dir);
   REQUIRE(!triangle.hit(ray, t_min, t_max));
 
-  ray_origin = path_tracer::Vec3(-2.6F, 0, -10);
-  dir = (path_tracer::Vec3(-2.6F, 0, 0) - ray_origin).normalize();
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(-2.6F, 0, -10);
+  dir = (Vec3(-2.6F, 0, 0) - ray_origin).normalize();
+  ray = Ray(ray_origin, dir);
   REQUIRE(!triangle.hit(ray, t_min, t_max));
 
-  ray_origin = path_tracer::Vec3(0, -5.000001F, -10);
-  dir = (path_tracer::Vec3(0, -5.000001F, 0) - ray_origin).normalize();
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(0, -5.00001F, -10);
+  dir = (Vec3(0, -5.00001F, 0) - ray_origin).normalize();
+  ray = Ray(ray_origin, dir);
   REQUIRE(!triangle.hit(ray, t_min, t_max));
 
-  ray_origin = path_tracer::Vec3(0, -4.999999F, -10);
-  dir = (path_tracer::Vec3(0, -4.999999F, 0) - ray_origin).normalize();
-  ray = path_tracer::Ray(ray_origin, dir);
+  ray_origin = Vec3(0, -4.999999F, -10);
+  dir = (Vec3(0, -4.999999F, 0) - ray_origin).normalize();
+  ray = Ray(ray_origin, dir);
   REQUIRE(triangle.hit(ray, t_min, t_max));
 
   // generate a bunch of random rays through the origin and
   // ensure they all hit
   for (int i = 0; i < 10; i++) {
-    auto const rand_point = path_tracer::Vec3::rand() * 1000;
-    auto const dir_to_origin =
-        (path_tracer::Vec3(0, 0, 0) - rand_point).normalize();
-    auto const r = path_tracer::Ray(rand_point, dir_to_origin);
+    const auto rand_point = Vec3::rand() * 1000;
+    const auto dir_to_origin = (Vec3(0, 0, 0) - rand_point).normalize();
+    const auto r = Ray(rand_point, dir_to_origin);
     INFO("Expecting vec to hit: " << r);
-    auto const hit = triangle.hit(r, t_min, t_max);
+    const auto hit = triangle.hit(r, t_min, t_max);
     REQUIRE(hit);
   }
 
@@ -140,12 +160,12 @@ TEST_CASE("path_tracer::Ray/Triangle intersection",
   // technically there is a chance that one of these could hit but
   // it's very unlikely. probably still bad testing practice but oh well
   for (int i = 0; i < 10; i++) {
-    auto const r1 = path_tracer::Vec3::rand() * 10000;
-    auto const r2 = path_tracer::Vec3::rand() * 10000;
-    auto const r_dir = (r1 - r2).normalize();
-    auto const r = path_tracer::Ray(r1, r_dir);
+    const auto r1 = Vec3::rand() * 10000;
+    const auto r2 = Vec3::rand() * 10000;
+    const auto r_dir = (r1 - r2).normalize();
+    const auto r = Ray(r1, r_dir);
     INFO("Expecting vec to miss: " << r);
-    auto const hit = triangle.hit(r, t_min, t_max);
+    const auto hit = triangle.hit(r, t_min, t_max);
     REQUIRE(!hit);
   }
 }
